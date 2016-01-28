9234702e5eea9d779681077beb97a3cc6c19f8fa
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-01-28 16:46:52 +0200

    More lenses.

--- src/todomvc.js
+++ src/todomvc.js
@@ -1,42 +1,44 @@
-import Bacon    from "baconjs"
-import R        from "ramda"
-import React    from "react"
-import ReactDOM from "react-dom"
-import Atom     from "bacon.atom"
+import Atom from "bacon.atom"
 import B, {bind, classes} from "bacon.react.html"
-
-const model = init => {
-  const all = Atom(init.map((item, id) => ({...item, id})))
-  let counter = init.length
-  return {
-    all,
-    isEmpty: B(all, a => a.length === 0),
-    active: B(all, R.filter(i => !i.completed)),
-    completed: B(all, R.filter(i => i.completed)),
-    addItem: title =>
-      all.modify(R.append({id: counter++, title, completed: false})),
-    setItem: ({id, title, completed}) =>
-      all.modify(R.map(i => i.id === id ? {id, title, completed} : i)),
-    remItem: ({id}) => all.modify(R.filter(i => i.id !== id)),
-    allDone: all.lens(R.lens(
-      R.all(i => i.completed),
-      (completed, items) => items.map(i => ({...i, completed})))),
-    clean: () => all.modify(R.filter(i => !i.completed))
-  }
-}
+import Bacon from "baconjs"
+import L from "partial.lenses"
+import R from "ramda"
+import React from "react"
+import ReactDOM from "react-dom"
+import uuid from "uuid"
+import {matches} from "schemation"
 
 const hash = Bacon.fromEvent(window, "hashchange").toProperty(0)
              .map(() => window.location.hash)
 
-const web = m => {
+const TodoItem = ({model, editing = Atom(false)}) =>
+  <B.li {...classes(B(model.lens(L("completed")), c => c && "completed"),
+                    B(editing, e => e && "editing"))}>
+    <B.input className="toggle" type="checkbox"
+             {...bind({checked: model.lens(L("completed"))})}/>
+    <B.label onDoubleClick={() => editing.set(true)}
+             className="view">{model.lens(L("title"))}</B.label>
+    <button className="destroy" onClick={() => model.set()}/>
+    {B(editing, e => e && (() => {
+      const exit = () => editing.set(false)
+      const save = e =>
+        {const newTitle = e.target.value.trim()
+         exit()
+         newTitle === "" ? model.set()
+                         : model.lens(L("title")).set(newTitle)}
+      return <B.input type="text" onBlur={save} className="edit" key="x"
+               mount={c => c && c.focus()} defaultValue={model.lens(L("title"))}
+               onKeyDown={e => e.which === 13 && save(e) ||
+                               e.which === 27 && exit()}/>})())}
+  </B.li>
+
+const TodoApp = ({model: m}) => {
   const routes = [{hash: "#/",          items: m.all,       title: "All"},
                   {hash: "#/active",    items: m.active,    title: "Active"},
                   {hash: "#/completed", items: m.completed, title: "Completed"}]
 
   const route = B(hash, h => R.find(r => r.hash === h, routes) || routes[0])
-  const items = route.flatMapLatest(r => r.items)
-
-  const editing = Atom(null)
+  const ids = B(route.flatMapLatest(r => r.items), R.map(R.prop("id")))
 
   return <div>
     <section className="todoapp">
@@ -46,31 +48,13 @@ const web = m => {
            placeholder="What needs to be done?" onKeyDown={e => {
              const t = e.target.value.trim()
              if (e.which === 13 && t !== "") {
-               m.addItem(t); e.target.value = ""}}}/>
+               m.addItem({title: t}); e.target.value = ""}}}/>
       </header>
       <section className="main">
         <B.input type="checkbox" className="toggle-all" hidden={m.isEmpty}
           {...bind({checked: m.allDone})}/>
-        <B.ul className="todo-list">{B(items, R.map(({id, title, completed}) =>
-          <B.li key={id} {...classes(completed && "completed",
-                                     B(editing, e => e === id && "editing"))}>
-            <input className="toggle" type="checkbox" checked={completed}
-              onChange={() => m.setItem({id, title, completed: !completed})}/>
-            <label onDoubleClick={() => editing.set(id)}
-                   className="view">{title}</label>
-            <button className="destroy" onClick={() => m.remItem({id})}/>
-            {B(editing, e => e === id && (() => {
-              const exit = () => editing.set(null)
-              const save = e =>
-                {const newTitle = e.target.value.trim()
-                 exit()
-                 newTitle === "" ? m.remItem({id})
-                                 : m.setItem({id, title: newTitle, completed})}
-              return <B.input type="text" onBlur={save} className="edit" key="x"
-                       mount={c => c && c.focus()} defaultValue={title}
-                       onKeyDown={e => e.which === 13 && save(e) ||
-                                       e.which === 27 && exit()}/>})())}
-          </B.li>))}</B.ul>
+        <B.ul className="todo-list">{B(ids, R.map(id =>
+          <TodoItem key={id} model={m.all.lens(L.find(matches({id})))}/>))}</B.ul>
       </section>
       <B.footer className="footer" hidden={m.isEmpty}>
         <B.span className="todo-count">{B(m.active,
@@ -88,9 +72,22 @@ const web = m => {
   </div>
 }
 
+TodoApp.model = (all = Atom([])) => ({
+  all,
+  isEmpty: B(all, a => a.length === 0),
+  active: B(all, R.filter(i => !i.completed)),
+  completed: B(all, R.filter(i => i.completed)),
+  addItem: ({id = uuid.v4(), title, completed = false}) =>
+    all.modify(R.append({id, title, completed})),
+  allDone: all.lens(L.lens(
+    R.all(i => i.completed),
+    (completed, items) => items.map(i => ({...i, completed})))),
+  clean: () => all.modify(R.filter(i => !i.completed))
+})
+
 const storeKey = "todos-react.bacon"
 
-const m = model(JSON.parse(localStorage.getItem(storeKey) || "[]"))
+const m = TodoApp.model(Atom(JSON.parse(localStorage.getItem(storeKey) || "[]")))
 m.all.onValue(is => localStorage.setItem(storeKey, JSON.stringify(is)))
 
-ReactDOM.render(web(m), document.getElementById("app"))
+ReactDOM.render(<TodoApp model={m}/>, document.getElementById("app"))
